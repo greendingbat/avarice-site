ec73aed068f8a2fda1328ec0068085882485f6ff
greendingbat <matthew-burns@live.com>
2024-08-09 16:49:19 -0600

    scrolling text

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,43 @@ import { EIGHTFOLD } from "./consts";
 import { getMessage } from "./api";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message1, setMessage1] = useState("");
+  const [message2, setMessage2] = useState("");
   useEffect(() => {
-    getMessage().then((r) => {setMessage(r); setLoading(false)});
+    getMessage().then((r) => {
+      setMessage1(r);
+    });
+    getMessage().then((r) => {
+      setMessage2(r);
+      setLoading(false);
+      setInterval(() => {
+        let messageDisplayed = 1;
+        getMessage().then((r) => {
+          if (messageDisplayed == 1) {
+            setMessage2(r);
+            messageDisplayed = 2;
+          }
+          if (messageDisplayed == 2) {
+            setMessage1(r);
+            messageDisplayed = 1;
+          }
+          console.log(messageDisplayed);
+        });
+      }, 5 * 60000);
+    });
   }, []);
 
-  return !loading && (
-    <>
-      <h1>MESSAGES FROM ADAM.</h1>
-      <p>{message}</p>
-      <p>{EIGHTFOLD}</p>
-    </>
+  return (
+    !loading && (
+      <>
+        <h1>MESSAGES FROM ADAM.</h1>
+        <div className="moving-text">
+          <div className="message-container1">{message1}</div>
+          <div className="message-container2">{message2}</div>
+        </div>
+      </>
+    )
   );
 }
 
